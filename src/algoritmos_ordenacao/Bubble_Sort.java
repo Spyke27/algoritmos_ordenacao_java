1cd261ecacbe424c96f4a4fcbb2a5a00528af770
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-22 22:01:51 -0300

    bubble_sort

--- src/algoritmos_ordenacao/Bubble_Sort.java
+++ src/algoritmos_ordenacao/Bubble_Sort.java
@@ -3,7 +3,21 @@ package algoritmos_ordenacao;
 public class Bubble_Sort {
 
 	public static void main(String[] args) {
-		Integer[] arr = {8, 5, 3, 9, 7, 2, 1, 4};
+		Integer[] arr = {8, 5, 3, 9, 7, 2, 1, 4, 6};
+		
+		int i;
+		int j;
+		int aux;
+		
+		for(i = 0; i < arr.length - 1; i++) {
+			for(j = 0; j < arr.length - 1; j++) {
+				if(arr[j] > arr[j + 1]) {
+					aux = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = aux;
+				}
+			}
+		}
 		
 		for(Integer num: arr) {
 			System.out.println(num);
