652947dd949e2ff724b6ad7a024b283d84fa1faf
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-22 21:37:11 -0300

    starting project

---
+++ src/algoritmos_ordenacao/Bubble_Sort.java
@@ -0,0 +1,14 @@
+package algoritmos_ordenacao;
+
+public class Bubble_Sort {
+
+	public static void main(String[] args) {
+		Integer[] arr = {8, 5, 3, 9, 7, 2, 1, 4};
+		
+		for(Integer num: arr) {
+			System.out.println(num);
+		}
+
+	}
+
+}
